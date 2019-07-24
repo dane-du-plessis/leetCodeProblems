6d05019f7c8f08e3419f1042c855455c8f998579
dane.duplessis <dane.duplessis@entelect.co.za>
2019-07-24 17:12:00 +0200

    How to build a graph for a maze?

--- mrRobot.js
+++ mrRobot.js
@@ -5,27 +5,42 @@
 // 2. The exact path that is the sortest route.
 // 3. The maximum number of steps required to reach the destination cell from the origin.
 // 4. The longest possible path.
+let log = console.log
 
+class GraphNode {
+    constructor(){
+        this.outgoing = []
+    }
+}
+
+class Arc {
+    constructor() {
+        this.start = null
+        this.end = null
+    }
+}
+
+// let grid = [
+//     [0,0,0,0,0,0],
+//     [0,1,1,1,1,0],
+//     [0,1,9,0,1,0],
+//     [0,1,1,0,0,0],
+//     [0,0,0,0,0,0]
+// ];
+
+// grid = [
+//     [0,0,0],
+//     [0,0,9]
+// ];
 
 let grid = [
-    [0,0,0,0,0,0],
-    [0,1,1,1,1,0],
-    [0,1,9,0,1,0],
-    [0,1,1,0,0,0],
-    [0,0,0,0,0,0]
-];
-
-grid = [
     [0,0,0],
     [0,1,1],
     [0,0,9]
 ];
 
-grid = [
-    [0,0,0],
-    [0,0,9]
-];
 
 let numRows = grid.length;
 let numCols = grid[0].length;
 
+// construct graph of the grid

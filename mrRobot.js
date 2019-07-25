09a2c643b4bb0405d320fae4cde90187ee63173c
Dane du Plessis <daneasc@gmail.com>
2019-07-25 18:14:22 +0200

    Mr Robot builds a graph from a 2d array.

--- mrRobot.js
+++ mrRobot.js
@@ -8,8 +8,11 @@
 let log = console.log
 
 class GraphNode {
-    constructor(){
-        this.outgoing = []
+    constructor(x,y){
+        this.x = x // beacuse this is a grid node
+        this.y = y
+        this.target = false // so that we know whether or not this is what we want
+        this.outgoing = [] // directed graph
     }
 }
 
@@ -20,27 +23,134 @@ class Arc {
     }
 }
 
-// let grid = [
-//     [0,0,0,0,0,0],
-//     [0,1,1,1,1,0],
-//     [0,1,9,0,1,0],
-//     [0,1,1,0,0,0],
-//     [0,0,0,0,0,0]
-// ];
+
+let grid = [
+    [0,0,0,0],
+    [0,1,1,1],
+    [0,1,9,0],
+    [0,1,1,0],
+    [0,0,0,0]
+];
 
 // grid = [
 //     [0,0,0],
 //     [0,0,9]
 // ];
 
-let grid = [
-    [0,0,0],
-    [0,1,1],
-    [0,0,9]
-];
-
+// const grid = [
+//     [0,0,0],
+//     [0,1,1],
+//     [0,0,9]
+// ];
 
 let numRows = grid.length;
 let numCols = grid[0].length;
 
-// construct graph of the grid
+// construct graph from the grid, this is really an adjacency list
+for (let i = 0; i < numRows; i++) {
+    let row = ''
+    for (let j = 0; j < numCols; j++) {
+        row += grid[i][j] + ' ' 
+    }
+    log(row)
+}
+
+// create a graph
+const root = new GraphNode(0,0)
+const allNodes = new Set() // a set containing all the nodes
+allNodes.add(root)
+
+let findNode = function(allNodes, x, y) {
+    for(n of allNodes) {
+        if (n.x === x && n.y === y) {
+            return n
+        }
+    }
+
+    return null
+}
+
+let checkAndAddNode = function(grid, allNodes, x, y) {
+    let existingNode  = findNode(allNodes,x,y)
+    if (existingNode) {
+        return existingNode
+    }
+
+    if (grid[x][y] === 0 || grid[x][y] === 9) {
+        let newNode = new GraphNode(x,y)
+        if (grid[x][y] === 9) {
+            newNode.target = true
+        }
+        allNodes.add(newNode)
+        return newNode
+    }
+
+    return null
+}
+
+
+for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+        log(i + " " + j)
+        // i, j
+        // if node is new, add it to the set.
+        let node = checkAndAddNode(grid, allNodes, i, j)
+        if (node === null) {
+            log('nothing here')
+            continue
+        }
+        log("node present")
+
+        let x
+        let y
+       
+        //Add edges to other nodes, if they're there
+
+        // up
+        // i-1, j
+        x = i-1
+        y = j
+        if (0 <= x && x < numRows) {
+            let otherNode = checkAndAddNode(grid, allNodes, x, y)
+            if (otherNode) {
+                node.outgoing.push(otherNode) // duplicates
+            } 
+        }
+
+        // down
+        // i+1, j
+        x = i+1
+        y = j
+        if (0 <= x && x < numRows) {
+            let otherNode = checkAndAddNode(grid, allNodes, x, y)
+            if (otherNode) {
+                node.outgoing.push(otherNode)
+            } 
+        }
+
+        // left
+        // i, j-1
+        x = i
+        y = j-1
+        if (0 <= x && x < numRows) {
+            let otherNode = checkAndAddNode(grid, allNodes, x, y)
+            if (otherNode) {
+                node.outgoing.push(otherNode)
+            } 
+        }
+
+        // right
+        // i, j+1
+        x = i
+        y = j+1
+        if (0 <= x && x < numRows) {
+            let otherNode = checkAndAddNode(grid, allNodes, x, y)
+            if (otherNode) {
+                node.outgoing.push(otherNode)
+            }
+        }
+    }
+}
+
+log(allNodes)
+log(allNodes.size)

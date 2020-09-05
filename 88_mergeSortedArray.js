509055cf4156b6ca4928dbb86d176e5e7688fccb
Dane du Plessis <dane@polymorph.co.za>
2020-09-05 11:38:36 +0200

    Programming is difficult. Even the easy things are difficult.

--- 88_mergeSortedArray.js
+++ 88_mergeSortedArray.js
@@ -6,20 +6,44 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    for (let i = 0; i < n; i++) {
-        // get first element from nums1
-        const num = num2[i]
-        // insert it in the right position in nums2
-        insert(num, nums2, m, n)
-    }
+    let actualLength = m
+    let lastInsertPosition = 0
+    nums2.forEach(e => {
+        let inserted = false
+        for (let j = lastInsertPosition; j < actualLength; j++) {
+            console.log(e + "  " + nums1[j])
+            if (e < nums1[j]) {
+                console.log("inserting at " + j)
+                insert(nums1, e, j)
+                lastInsertPosition = j
+                actualLength++
+                inserted = true
+                break
+            }
+        }
+        if(!inserted) {
+            insert(nums1, e, actualLength)
+            lastInsertPosition = actualLength
+            actualLength++
+        }
+
+    });
 };
 
-var insert = function (num, nums2, m, n) {
-    // here?
-    for (let j = 0; j < m + n; j++) {
+var insert = function (nums1, e, insertAt) {
+    // shift all the elements up one position - the one at the end falls off
+    for (let i = nums1.length - 2; i >= insertAt; i--) {
+        nums1[i + 1] = nums1[i]
     }
+    nums1[insertAt] = e
 }
 
 let nums1 = [1, 2, 3, 0, 0, 0]   // , m = 3
 let nums2 = [2, 5, 6]         // , n = 3
+
+merge(nums1, 3, nums2, nums2.length);
+
+// insert(nums1, 2.5, nums1.length-1)
+
+console.log(nums1)
 
